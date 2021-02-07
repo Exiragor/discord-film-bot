3c4fa2174e021e064dd384c9b41e9275cf0a2c20
Alexander Lebedev <exiragor@yandex.ru>
2021-02-07 18:55:38 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ const config = require('config');
 const {activateBot, useCommand} = require('./bot');
 
 activateBot(config.get('BOT_TOKEN'), async (bot) => {
-    // await useCommand(bot, 'filmId');
+    await useCommand(bot, 'filmId');
 });
-
-require('./Commands/FilmIdCommand')();
